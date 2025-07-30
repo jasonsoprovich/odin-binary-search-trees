5f4efd1cd4cb821a7ec76b08b5e6a1550e8a2e1c
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-07-29 19:34:41 -0600

    feat: add find method

--- index.js
+++ index.js
@@ -64,6 +64,16 @@ class Tree {
     return current;
   }
 
+  find(value, node = this.root) {
+    if (node === null) return new Node(value);
+    if (value === node.data) return node;
+    if (value < node.data) {
+      return node.left = this.find(value, node.left);
+    } else {
+      return node.right = this.find(value, node.right);
+    }
+  }
+
   prettyPrint = (node = this.root, prefix = '', isLeft = true) => {
   if (node === null) {
     return;
@@ -83,7 +93,13 @@ const testArray = [1, 2, 3, 5, 4];
 // const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(testArray);
 tree.prettyPrint();
+
+// test insert / delete
 tree.insert(6);
 tree.prettyPrint();
 tree.deleteItem(4);
 tree.prettyPrint();
+
+// test find
+console.log('Find 3', tree.find(3)); // found
+console.log('Find 99', tree.find(99)); // not found
